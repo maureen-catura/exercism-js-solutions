108f6207ffbca6b74223f25dad0c71cf2c83abbe
Maureen Catura <maureen@perkd.me>
2023-10-17 17:46:54 +0800

    add solution to lasagna-master exercise

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,43 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime = null) {
+    if (remainingTime === null) {
+        return 'You forgot to set the timer.';
+    } else if (remainingTime > 0) {
+        return 'Not done, please wait.';
+    }
+
+    return 'Lasagna is done.';
+}
+
+export function preparationTime(layers, preparationTime = 2) {
+    return layers.length * preparationTime;
+}
+
+export function quantities(layers) {
+    let quantities = { noodles: 0, sauce: 0 };
+
+    layers.forEach((layer) => {
+        if (layer === 'noodles') quantities.noodles += 50;
+        else if(layer === 'sauce') quantities.sauce += 0.2;
+    });
+
+    return quantities;
+}
+
+export function addSecretIngredient(friendsList, myList) {
+    let secretIngredient = friendsList[friendsList.length - 1];
+    myList.push(secretIngredient);
+}
+
+export function scaleRecipe(recipe, numberOfPortions) {
+    let scaledRecipe = {};
+
+    for (let key in recipe) {
+    scaledRecipe[key] = recipe[key] * (numberOfPortions / 2);
+    }
+
+    return scaledRecipe;
+}
